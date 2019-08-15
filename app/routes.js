104e10ec6b8b01f9db4071ff0f4dbf0e458a21e1
Kellie Matheson <kelliematheson@Kellies-MacBook.local>
2019-08-15 08:09:49 +0100

    Release version 1.3 Clean up syntax

--- app/routes.js
+++ app/routes.js
@@ -4,21 +4,21 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Branching
-router.post('/iteration-august-12/legal/detail', function (req, res) {
+router.post('/iteration-august-15/legal/detail', function (req, res) {
     let over18 = req.session.data['legal']
     if (over18 === 'false') {
-        res.redirect('/iteration-august-12/non-legal/detail')
+        res.redirect('/iteration-august-15/non-legal/detail')
     } else {
-        res.redirect('/iteration-august-12/legal/detail')
+        res.redirect('/iteration-august-15/legal/detail')
     }
 })
 
-router.post('/iteration-august-8/legal/length', function (req, res) {
+router.post('/iteration-august-12/legal/length', function (req, res) {
     let over18 = req.session.data['legal']
     if (over18 === 'false') {
-        res.redirect('/iteration-august-8/non-legal/length')
+        res.redirect('/iteration-august-12/non-legal/length')
     } else {
-        res.redirect('/iteration-august-8/legal/length')
+        res.redirect('/iteration-august-12/legal/length')
     }
 })
 
